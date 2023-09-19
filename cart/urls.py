ebce5b079425d0ba9a9b34a1d9cb4e02a1d23354
Morshed <morshednayeem@icloud.com>
2023-09-19 20:29:07 +0600

    5th commit

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('add-to-cart/<int:product_id>/',AddToCart.as_view(), name ='add-to-cart'),
-    path('cart-details/',CartView.as_view(), name = 'cart-details')
+    path('cart-details/',CartView.as_view(), name = 'cart-details'),
+    path('add-coupon/',AddCoupon.as_view(), name = 'add-coupon')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from django.utils import timezone
 from django.contrib import messages
 from typing import Any
 from django import http
@@ -5,6 +7,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404,redirect
 from .carts import Cart
 from product.models import Product
+from .models import Coupon
 from django.views.generic import View,TemplateView
 # Create your views here.
 
@@ -43,3 +46,36 @@ class CartView(TemplateView):
             return redirect ('cart-details')
 
         return super().get(request, *args, **kwargs)
+    
+
+class AddCoupon(View):
+    def post(self, *args, **kwargs):
+        code = self.request.POST.get('coupon', '')
+        coupon = Coupon.objects.filter(code__iexact = code, active=True)
+        cart = Cart(self.request)
+
+        if coupon.exists():
+            coupon = coupon.first()
+            current_date = datetime.date(timezone.now())
+            active_date = coupon.active_date
+            expiry_date = coupon.expiry_date
+
+            if current_date > expiry_date:
+                messages.warning(self.request,"The coupon has been expired!")
+                return redirect('cart-details')
+            
+            if current_date < active_date:
+                messages.warning(self.request,"The coupon is yet to be available!")
+                return redirect('cart-details')
+            
+            if cart.total() < coupon.required_amount_to_use_coupon:
+                messages.warning(self.request,f"You have to shop at least{coupon.required_amount_to_use_coupon} to use this coupon code")
+                return redirect('cart-details')
+            
+            cart.add_coupon(coupon.id)
+            messages.success(self.request,"Your coupon has been applied successfully!")
+            return redirect('cart-details')
+        
+        else:
+            messages.error(self.request,"Invalid coupon code!")
+            return redirect('cart-details')
